2787c888332129445085db3fed9079850c816627
Didier Alejandro Fajardo Annear <didierfajardo28@hotmail.com>
2024-01-08 23:03:18 -0500

    Add files via upload

---
+++ src/data/info.ts
@@ -0,0 +1,131 @@
+export const info = {
+  baseUrl: "https://github.com/AlejandroFajardo",
+  name: "Alejandro Fajardo",
+  jobDescription: "Web Developer",
+  about: `I'm a passionate web developer and computer and systems engineering student.
+  `,
+
+  experience: [
+    {
+      name: "CONCEPTO DISEÑO Y PUBLICIDAD",
+      location: "Santander, Colombia",
+      startDate: "NOV 2022",
+      endDate: "APR 2023",
+      description: [
+        "I dedicated myself mainly to the development of shopping cart type web pages, landing pages and advertising for clients.",
+        "React, Astro, MySQL, NodeJS, Boostrap, Tailwind, mongoDB"
+      ],
+    },
+
+    {
+      name: "NFF CONTRUCTIONS",
+      location: "Bogota, Colombia",
+      startDate: "JUN 2022",
+      endDate: "OCT 2022",
+      description: [
+        "Developing code in accordance with appropriate design and architectural rules",
+        "Documenting, testing, deployment, maintenance, databases and solutions",
+        "collaborated with other colleagues",
+        "Others tasks connected to the nature of the job",
+        "React, firebaseDB, Boostrap, NodeJS, Express"
+      ],
+    },
+
+    {
+      name: "MISION TIC 2022",
+      location: "Colombia",
+      startDate: "MAR 2021",
+      endDate: "DEC 2021",
+      description: [
+        "An intensive and Immersive Internship Program of the ICT ministry of Colombia, that prepares me for the world's most in-demand career. A 9 months program completely based on learning and challenges.",
+        "The program focused on the creation of different groups where each of these had a tutor and classes that ranged from the most basic of programming to developing an application where different web development technologies and agile development methodologies would be used, in this case Scrum",
+        "React, JAVA, Python, JavaScript, TypeScript, SCRUM, MySQL, Firebase"
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Pedagogical and Technological University of Colombia (UPTC)",
+      location: "Tunja, Boyaca, Colombia",
+      startDate: " ",
+      endDate: "In progress",
+      description: [
+        "Computer and systems engineering",
+      ],
+    },
+    {
+      name: "Training program, programming skills, web applications development",
+      location: "UdeA, Medellin, Antioquia, Colombia",
+      startDate: "2021",
+      endDate: "",
+      description: ["Intensive program of the Ministry of ICT of Colombia, focused on learning programming and web development skills"],
+    },
+    {
+      name: "Master in JavaScript",
+      location: "Udemy",
+      startDate: "2022",
+      endDate: " ",
+      description: ["Javascript, Jquery, Angular, and Node.js"],
+    },
+    {
+      name: "Master in PHP",
+      location: "Udemy",
+      startDate: "2022",
+      endDate: " ",
+      description: ["PHP, SQL, MVC, Laravel, Symfony, and WordPress"],
+    },
+    {
+      name: "Master in web development",
+      location: "Udemy",
+      startDate: "2022",
+      endDate: " ",
+      description: ["Angular, Laravel, Symfony, and Node.js"],
+    },
+  ],
+
+  socialMedia: {
+    twitter: "https://twitter.com/dafajardoa",
+    github: "https://github.com/AlejandroFajardo",
+    email: "mailto:dafadio19@gmail.com",
+    linkedin: "https://www.linkedin.com/in/dalejandrofajardoa/",
+  },
+
+  projects: [
+    {
+      title: "Spanish Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/portafolio.png",
+      githubUrl: "https://github.com/AlejandroFajardo/spanish-portfolio",
+      liveUrl: "https://spanish-portfolio.netlify.app/",
+    },
+    {
+      title: "La velada - clone page",
+      isFeatured: true,
+      thumbnail: "/assets/images/velada.png",
+      githubUrl: "https://github.com/AlejandroFajardo/LaVelada3-clone",
+      liveUrl: "https://velada-clone.netlify.app/",
+    },
+    {
+      title: "Hispanic timezones",
+      isFeatured: true,
+      thumbnail: "/assets/images/time.png",
+      githubUrl: "https://github.com/AlejandroFajardo/CRUDFirebase",
+      liveUrl: "https://alejandrofajardo.github.io/CRUDFirebase/",
+    },
+    {
+      title: "Tasks application",
+      isFeatured: true,
+      thumbnail: "/assets/images/task.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Mision TIC 2022 project",
+      isFeatured: true,
+      thumbnail: "/assets/images/pdt.png",
+      githubUrl: "https://github.com/AlejandroFajardo/MisionTIC2022",
+      liveUrl: "https://alejandrofajardo.github.io/MisionTIC2022/",
+    },
+  ],
+};
